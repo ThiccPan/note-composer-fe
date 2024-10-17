cd12e685024dab6564029e4e54619fb9d2ad31e5
ThiccPan <ihilmiderian@gmail.com>
2024-10-18 02:08:34 +0700

    feat: setup shadcn-vue, typescript, zod, tailwind

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,33 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
+  devtools: { enabled: true },
+
+  typescript: {
+    typeCheck: true,
+    strict: true,
+    tsConfig: {
+      compilerOptions: {
+        baseUrl: '.'
+      }
+    },
+  },
+
+  tailwindcss: {
+    editorSupport: true
+    // editorSupport: { autocompleteUtil: { as: 'tailwindClasses' } }
+  },
+
+  modules: ['@nuxtjs/tailwindcss', 'shadcn-nuxt', '@nuxtjs/color-mode'],
+  shadcn: {
+    /**
+     * Prefix for all the imported component
+     */
+    prefix: '',
+    /**
+     * Directory that the component lives in.
+     * @default "./components/ui"
+     */
+    componentDir: './components/ui'
+  }
 })
