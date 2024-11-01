7d44f9cf787b9ed8f93aafac6451f43d738e324c
ThiccPan <ihilmiderian@gmail.com>
2024-11-01 21:38:28 +0700

    feat: add more filtering to fetch notes

--- repository/Notes.ts
+++ repository/Notes.ts
@@ -1,18 +1,21 @@
 import type { addNoteType, updateNoteType } from "~/types/NoteDTO"
 import type { Note } from "~/types/types"
 
-const url = "http://localhost:8000/v1/notes"
+const url = "http://localhost:8000/v1"
 
 const fetchNotesReq = async (reqData: {
   token: string
   limit?: number
   order?: "asc" | "desc"
   search?: string
+  tagsId?: string[]
+  after?: string
+  before?: string
 }) => {
   const { data, error } = await useFetch<{
     message: string,
     data: Note[]
-  }>(url, {
+  }>(`${url}/notes`, {
     method: "get",
     headers: {
       'Authorization': reqData.token
@@ -21,14 +24,18 @@ const fetchNotesReq = async (reqData: {
       limit: reqData.limit,
       order: reqData.order,
       search: reqData.search,
+      tagsId: reqData.tagsId,
+      after: reqData.after,
+      before: reqData.before,
     }
   })
   if (error.value) {
     throw createError({
       ...error.value,
-      statusMessage: `Could not fetch data from ${url}`,
+      statusMessage: `Could not fetch data from ${url}/notes`,
     })
   }
+  console.log('data: ', data.value?.data)
   return data
 }
 
@@ -39,7 +46,7 @@ const fetchNoteByIdReq = async (reqData: {
   const { data, error } = await useFetch<{
     message: string,
     data: Note
-  }>(`${url}/${reqData.noteId}`, {
+  }>(`${url}/notes/${reqData.noteId}`, {
     method: "get",
     headers: {
       'Authorization': reqData.token
@@ -49,7 +56,7 @@ const fetchNoteByIdReq = async (reqData: {
   if (error.value) {
     throw createError({
       ...error.value,
-      statusMessage: `Could not fetch data from ${url}`,
+      statusMessage: `Could not fetch data from ${url}/notes`,
     })
   }
   return data
@@ -61,7 +68,7 @@ const addNoteReq = async (reqData: {
 }) => {
   const { data, error } = await useFetch<{
     message: string,
-  }>(url, {
+  }>(`${url}/notes`, {
     method: "post",
     headers: {
       'Authorization': reqData.token
@@ -71,7 +78,7 @@ const addNoteReq = async (reqData: {
   if (error.value) {
     throw createError({
       ...error.value,
-      statusMessage: `Could not add data from ${url}`,
+      statusMessage: `Could not add data from ${url}/notes`,
     })
   }
   console.log("added data: ", data)
@@ -86,7 +93,7 @@ const updateNoteReq = async (reqData: {
   console.log("updating note", reqData.noteId)
   const { data, error } = await useFetch<{
     message: string,
-  }>(`${url}/${reqData.noteId}`, {
+  }>(`${url}/notes/${reqData.noteId}`, {
     method: "put",
     headers: {
       'Authorization': reqData.token
@@ -97,7 +104,7 @@ const updateNoteReq = async (reqData: {
   if (error.value) {
     throw createError({
       ...error.value,
-      statusMessage: `Could not update data from ${url}`,
+      statusMessage: `Could not update data from ${url}/notes`,
     })
   }
   return data
@@ -109,7 +116,7 @@ const deleteNoteReq = async (reqData: {
 }) => {
   const { data, error } = await useFetch<{
     message: string,
-  }>(`${url}/${reqData.noteId}`, {
+  }>(`${url}/notes/${reqData.noteId}`, {
     method: "delete",
     headers: {
       'Authorization': reqData.token
